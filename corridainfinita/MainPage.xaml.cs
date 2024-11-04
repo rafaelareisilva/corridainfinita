b935e5011efc48bbd74894d7a10a6bb4f3e794e6
rafa <reiisrafaela@icloud.com>
2024-11-04 11:48:08 -0300

    04/11

--- corridainfinita/MainPage.xaml.cs
+++ corridainfinita/MainPage.xaml.cs
@@ -29,7 +29,7 @@ public partial class MainPage : ContentPage
 	}
 	void CalculaVelocidade(double w)
 	{
-		velocidade1 = (int)(w * 0.001);
+		velocidade1 = (int)(w * 0.003);
 		velocidade2 = (int)(w * 0.004);
 		velocidade3 = (int)(w * 0.008);
 		velocidade = (int)(w * 0.01);
@@ -44,10 +44,49 @@ public partial class MainPage : ContentPage
 		foreach (var A in HSLayer3.Children)
 		(A as Image).WidthRequest = w;
 		
-		HSLayer1.WidthRequest = w * 1.5;
-		HSLayer2.WidthRequest = w * 1.5;
-		HSLayer3.WidthRequest = w * 1.5;
+		HSLayer1.WidthRequest = w ;
+		HSLayer2.WidthRequest = w ;
+		HSLayer3.WidthRequest = w ;
 	}
 
+	void GerenciaCenarios()
+	{
+		MoveCenario();
+		GerenciaCenarios (HSLayer1);
+		GerenciaCenarios (HSLayer2);
+		GerenciaCenarios (HSLayer3);
+		
+	}
+	void MoveCenario()
+	{
+		HSLayer1.TranslationX -= velocidade1;
+		HSLayer2.TranslationX -= velocidade2;
+		HSLayer3.TranslationX -= velocidade3;
+	}
+	void GerenciaCenarios(HorizontalStackLayout horizontalStackLayout)
+	{
+		var view = (horizontalStackLayout.Children.First() as Image);
+		if (view.WidthRequest + horizontalStackLayout.TranslationX < 0)
+		{
+			horizontalStackLayout.Children.Remove(view);
+			horizontalStackLayout.Children.Add(view);
+			horizontalStackLayout.TranslationX = view.TranslationX;
+		}
+	}
+
+	async Task Desenha()
+   {
+	    while(!estaMorto)
+	{
+		GerenciaCenarios();
+		await Task.Delay(tempoEntreFrames);
+	}
+   }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		Desenha();
+    }
 }
 
